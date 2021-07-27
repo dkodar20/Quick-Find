33d7b60b3270cc3ea91790862e820437b356f031
Rudr Tiwari <rudrtiwari@gmail.com>
2021-07-27 20:48:27 +0530

    Updated InputModule.py

--- InputModule.py
+++ InputModule.py
@@ -1,26 +1,58 @@
 import os
 import subprocess
 
-if subprocess.call(["g++", "./main.cc"]) == 0:
-    subprocess.call(["./a.out < input.txt > output.txt"], shell = True)
-    #taking input from output.txt
-    with open('output.txt', 'r') as file:
-        lines = [line.strip() for line in file]
 
-    fft = list(map(int, lines[1].split()))
-    kmp = list(map(int, lines[3].split()))
+def main():
+	if subprocess.call(["g++", "./main.cc"]) == 0:
+		subprocess.call(["./a.out < input.txt > output.txt"], shell = True)
+		#taking input from output.txt
+		with open('output.txt', 'r') as file:
+			lines = [line.strip() for line in file]
 
-    with open('input.txt', 'r') as file:
-        lines = [line.strip() for line in file]
-    text = lines[0].split()[0]
-    pattern = lines[1].split()[0]
+		fft = list(map(int, lines[1].split()))
+		kmp = list(map(int, lines[3].split()))
 
-    print(fft, kmp)
+		with open('input.txt', 'r') as file:
+			lines = [line.strip() for line in file]
+		text = lines[0].split()[0]
+		pattern = lines[1].split()[0]
 
-    m = len(pattern)
+		i = 0
+		n = len(text); m = len(pattern)
+		d = len(fft); j = 0
 
-    for i in fft:
-        print(text[i:i+m])
-    subprocess.call(["rm -rf ./output.txt"], shell = True);
-else:
-    print("Compilation errors");
+		print('Pattern found using FFT -')
+		while i < n:
+			if j < d and i == fft[j]:
+				for k in range(m):
+					print('\033[1m' + '\033[33m' + text[i+k], end='')
+				i += k+1
+				j += 1
+			else:
+				print('\033[0m' + '\033[1m' + text[i], end='')
+				i += 1
+
+		print()
+		i = 0; j = 0
+		d = len(kmp)
+
+		print('\033[0m' + 'Pattern found using KMP -')
+		while i < n:
+			if j < d and i == kmp[j]:
+				for k in range(m):
+					print('\033[1m' + '\033[33m' + text[i+k], end='')
+				i += k+1
+				j += 1
+			else:
+				print('\033[0m' + '\033[1m' + text[i], end='')
+				i += 1
+
+		print()
+		subprocess.call(["rm -rf ./output.txt"], shell = True);
+	else:
+		print("Compilation errors");
+
+
+if __name__ == "__main__":
+	main()
+
